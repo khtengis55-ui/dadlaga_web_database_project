564c89323c39b083749b61a2c252e6da16c3dea4
unknown <khtengis55@gmail.com>
2026-06-09 15:06:41 +0800

    1

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+import {    createClient    } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+
+const SUPABASE_URL = "https://hxedaogfbxxtjmnvfguk.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.hxedaogfbxxtjmnvfgukeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZWRhb2dmYnh4dGptbnZmZ3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NjQyMzAsImV4cCI6MjA5NjU0MDIzMH0.TW44-8Kgaj4shmaLXtFE8NZ91sUBGP2B0Tm-0C4bHio.TW44-8Kgaj4shmaLXtFE8NZ91sUBGP2B0Tm-0C4bHio"
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+if(supabase.auth){
+    console.log("holbogodsn bn")
+    console.log(supabase.auth)
+
+}
